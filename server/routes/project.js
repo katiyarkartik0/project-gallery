b1eb6a89df7da445f7d8e739d0c0b9bf39542ffe
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-09-24 22:01:18 +0530

    minor fix

--- server/routes/project.js
+++ server/routes/project.js
@@ -2,12 +2,10 @@ const express = require("express");
 const projectRoutes = express.Router();
 const bodyParser = require("body-parser");
 const { getProjects } = require("../controllers/project");
-const { searchProjects } = require("../controllers/smartSearch");
 
 projectRoutes.use(bodyParser.urlencoded({ extended: false }));
 projectRoutes.use(bodyParser.json());
 
 projectRoutes.get("/getProjects",getProjects)
-projectRoutes.get("/smartSearch",searchProjects)
 
 module.exports = { projectRoutes };
