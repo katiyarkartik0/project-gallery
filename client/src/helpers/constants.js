9f0ce68cbddead80bb161c200042858f36c56341
Kartik Katiyar <katiyarkartik0@gmail.com>
2024-02-19 13:24:37 +0530

    url error fix

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,6 +1,6 @@
 let ENDPOINT = "";
 if (process.env.NODE_ENV === "production") {
-  ENDPOINT = "https://project-gallery-l5ga.onrender.com/";
+  ENDPOINT = "https://project-gallery-l5ga.onrender.com";
 } else if (process.env.NODE_ENV === "development") {
   ENDPOINT = "http://localhost:3001";
 }
