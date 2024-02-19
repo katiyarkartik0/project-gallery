5a7aa680f205dff5ca2919cde9b5dc13d3a5d45f
Kartik Katiyar <katiyarkartik0@gmail.com>
2024-02-19 13:17:46 +0530

    backend route updated

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,1 +1,8 @@
-export const ENDPOINT = "https://project-gallery-production.up.railway.app"
+let ENDPOINT = "";
+if (process.env.NODE_ENV === "production") {
+  ENDPOINT = "https://project-gallery-l5ga.onrender.com/";
+} else if (process.env.NODE_ENV === "development") {
+  ENDPOINT = "http://localhost:3001";
+}
+
+export default ENDPOINT;
